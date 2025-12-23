40b0c5948129dd1e419bdaa272dfeb2bfbc04a9e
culdflow <fevedimitri@gmail.com>
2025-12-23 19:14:14 +0100

    nearly finished 5 ex00

---
+++ CPP_module_5/ex00/Bureaucrat.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h> 
+#include <iomanip>
+
+class Bureaucrat
+{
+	private:
+		std::string		_name;
+		int				_grade;
+	public:
+		Bureaucrat();
+		Bureaucrat( int grade, std::string name );
+		Bureaucrat( Bureaucrat& copy );
+		~Bureaucrat();
+
+		Bureaucrat&	operator=( const Bureaucrat& src );
+
+		std::string	get_name()const;
+		int			get_grade()const;
+
+		void		set_grade( int grade );
+
+		void		incr_grade();
+		void		decr_grade();
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+};
+
+std::ostream	&operator<<( std::ostream &o, Bureaucrat const &src );
